969936c8ed02a69b001440c043bc765632913f9e
jrholt26 <jrholt26@transy.edu>
2024-01-10 20:05:48 -0500

    Initial Commit

---
+++ GittestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GittestApp
+//
+//  Created by Jackson Holt on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GittestApp/GittestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GittestAppApp.swift
+//  GittestApp
+//
+//  Created by Jackson Holt on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GittestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
